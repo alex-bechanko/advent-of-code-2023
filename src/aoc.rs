6f21a021179fae443f491cd5a6ad3d74730f7783
Alex Bechanko <alexbechanko@gmail.com>
2024-05-28 02:54:07 -0400

    wip

--- src/aoc.rs
+++ src/aoc.rs
@@ -77,7 +77,7 @@ impl SolutionDay {
 
 impl std::fmt::Display for SolutionDay {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}", self.0)
+        self.0.fmt(f)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,17 @@
 mod aoc;
 
 use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
 use std::{path::PathBuf, str::FromStr, time::Duration};
 
 const DEFAULT_FOLDER: &str = "inputs";
+
+const TOTAL_GOAL_TIME_MS: u64 = 60000;
+
+const SOLUTION_GOAL_TIME_MS: u64 = 1000;
+const SOLUTION_GOAL_TIME_GREEN_THRESHOLD: u64 = 200;
+const SOLUTION_GOAL_TIME_YELLOW_THRESHOLD: u64 = 500;
+
 
 #[derive(Debug, Parser)]
 #[command(name = "aoc")]
@@ -103,35 +111,89 @@ fn run_day(args: DayArgs) -> () {
     }
 }
 
+fn print_all_header() -> () {
+    println!(
+        "{:<4}{:<6}{:<9}{:<8}{}",
+        "Day", "Part", "Time", "Result", "Answer"
+    );
+}
+
+fn print_day_fail(day: aoc::SolutionDay, e: aoc::RunError) -> () {
+    println!("{:<4}{:<6}{:<9}{:<8}{}", day, "n/a", "n/a", "FAIL".red(), e)
+}
+
+fn print_day_pass(day: aoc::SolutionDay, res: aoc::DayResult) -> () {
+
+    let part_a_pretty_duration = color_formatted_duration(&res.part_a_duration);
+    println!(
+        "{:<4}{:<6}{:>7}{:>8}{}",
+        day,
+        aoc::SolutionPart::PartA,
+        part_a_pretty_duration,
+        "PASS".green(),
+        res.part_a_result,
+    );
+
+    let part_b_pretty_duration = color_formatted_duration(&res.part_b_duration);
+
+    println!(
+        "{:<4}{:<6}{:>7}{:>8}{}",
+        "",
+        aoc::SolutionPart::PartB,
+        part_b_pretty_duration,
+        "PASS".green(),
+        res.part_b_result,
+    );
+}
+
+fn print_total_duration(d: &Duration) -> () {
+    let formatted = if *d < Duration::from_millis(TOTAL_GOAL_TIME_MS) {
+        format_duration(d).green()
+    } else {
+        format_duration(d).red()
+    };
+
+
+    println!("\nTotal Duration .... {}", formatted);
+}
+
+fn color_formatted_duration(d: &Duration) -> colored::ColoredString {
+    let goal = Duration::from_millis(SOLUTION_GOAL_TIME_MS);
+    let green = Duration::from_millis(SOLUTION_GOAL_TIME_GREEN_THRESHOLD);
+    let yellow = Duration::from_millis(SOLUTION_GOAL_TIME_YELLOW_THRESHOLD);
+
+    let formatted = format_duration(d);
+    let diff = *d - goal;
+
+    if diff < green {
+        formatted.green()
+    } else if diff < yellow {
+        formatted.yellow()
+    } else {
+        formatted.red()
+    }
+}
+
 fn run_all(args: AllArgs) -> () {
     let default_folder = PathBuf::from_str(DEFAULT_FOLDER).unwrap();
     let input_folder = args.input_folder.unwrap_or(default_folder);
 
     let mut total_duration = std::time::Duration::ZERO;
+    print_all_header();
     for d in 1..26 {
         let day = aoc::SolutionDay::new(d).unwrap();
         let input = aoc::day_file(&input_folder, day);
 
         match aoc::solve_day(day, &input) {
-            Err(e) => println!("Day {} Error: {}", day, e),
+            Err(e) => print_day_fail(day, e),
             Ok(res) => {
                 total_duration += res.part_a_duration + res.part_b_duration;
-                println!(
-                    "Day {} Part A: {} .... {}",
-                    day,
-                    res.part_a_result,
-                    format_duration(&res.part_a_duration)
-                );
-                println!(
-                    "       Part B: {} .... {}",
-                    res.part_b_result,
-                    format_duration(&res.part_b_duration)
-                );
-                
+                print_day_pass(day, res);
             }
         }
     }
-    println!("Total Duration .... {}", format_duration(&total_duration));
+
+    print_total_duration(&total_duration);
 }
 
 fn main() {
